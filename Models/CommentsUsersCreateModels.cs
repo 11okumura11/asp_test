342e8e7375a6f3bef4a1572c8cbb6dcf0550d6a3
FRONTARC\naoki.okumura <naoki.okumura@frontarc.co.jp>
2022-04-22 16:27:37 +0900

    不要なmodelの削除

--- Models/CommentsUsersCreateModels.cs
+++ Models/CommentsUsersCreateModels.cs
@@ -6,7 +6,6 @@ namespace asp_test.Models
     public class CommentsUsersCreateModels
     {
         public List<Data.User>? Users { get; set; }
-        public SelectList? Uid { get; set; }
         public int Userid { get; set; }
         public string Comment1 { get; set; } = null!;
 
